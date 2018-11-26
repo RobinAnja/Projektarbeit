a887a03a290020ddc98d751c57b6da8ce2af7159
Robin <Robin@MAINPC>
2018-11-26 20:11:48 +0100

    Einbindung von Dateien, Formatiierung, Responsive design und erste Konsolenausgaben; Auslagerung der Logik in eine externe datei

---
+++ GUI/script.js
@@ -0,0 +1,13 @@
+	$(document).ready(function(){
+		/* Hier der jQuery-Code */
+		console.log('Hallo Welt');
+		/* Hier Daten einbinden 
+		
+		Beispiel an JSON Datei: 
+		$.getJSON("Pfad der datei", function(data){
+		
+			//Verarbeiten der Daten
+		});
+		
+		*/
+	});
